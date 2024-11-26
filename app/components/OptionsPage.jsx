ebff00d945656d9e15952a53d46b7548175dc3a3
Vraj Vyas <vyasvraj92@gmail.com>
2024-11-26 20:44:05 +0530

    fixed the new landing page and added ss of it

--- app/components/OptionsPage.jsx
+++ app/components/OptionsPage.jsx
@@ -9,103 +9,68 @@ import CompressPDF from "./CompressPDF";
 const AddPages = dynamic(() => import('./AddPages'), { ssr: false });
 import GetPDFImages from "./GetPDFImages";
 const OptionsPage = ({ handleButtonClick, activeSection }) => {
+  const cardData = [
+    {
+      id: 'merge',
+      imageSrc: 'https://img.icons8.com/doodle/48/000000/merge-files.png',
+      title: 'Merge PDF',
+      description: 'Combine PDFs effortlessly.',
+    },
+    {
+      id: 'edit',
+      imageSrc: 'https://img.icons8.com/doodle/48/000000/edit.png',
+      title: 'Edit PDF',
+      description: 'Modify PDFs quickly and easily.',
+    },
+    {
+      id: 'image',
+      imageSrc: 'https://img.icons8.com/?size=100&id=67369&format=png&color=000000',
+      title: 'Image to PDF',
+      description: 'Convert images to PDF effortlessly.',
+    },
+    {
+      id: 'compress',
+      imageSrc: 'https://img.icons8.com/48/ffffff/compress.png',
+      title: 'Compress PDF',
+      description: 'Shrink PDFs for easy sharing.',
+    },
+    {
+      id: 'addpages',
+      imageSrc: '/addpages.png',
+      title: 'Add Pages to PDF',
+      description: 'Insert new pages to your PDF.',
+    },
+    {
+      id: 'extract',
+      imageSrc: 'https://img.icons8.com/?size=100&id=wUqJRyU3Sxwa&format=png&color=000000',
+      title: 'Extract Images',
+      description: 'Easily extract images from PDFs.',
+    },
+  ];
   return (
-    <div className="min-h-screen relative z-40   ">
+    <div className="min-h-screen relative z-40 -mt-12 md:-mt-0  ">
       <div className="bg-transparent w-full py-12 px-4">
         <div className="max-w-7xl mx-auto text-center">
           <div className="text-4xl font-bold text-white mb-2">PDF Manipulator Suite</div>
           <p className="text-lg text-gray-500 mb-6">Edit, compress, merge, convert, and more—all in one tool!</p>
-          <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('merge')}
-            >
-              <Image
-                src="https://img.icons8.com/doodle/48/000000/merge-files.png"
-                alt="Merge PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Merge PDF</h3>
-              <p className="text-gray-300 text-center">Combine PDFs effortlessly.</p>
-            </div>
-
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('edit')}
-            >
-              <Image
-                src="https://img.icons8.com/doodle/48/000000/edit.png"
-                alt="Edit PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Edit PDF</h3>
-              <p className="text-gray-300 text-center">Modify PDFs quickly and easily.</p>
-            </div>
-
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('image')}
-            >
-              <Image
-                src="https://img.icons8.com/?size=100&id=67369&format=png&color=000000"
-                alt="Image to PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Image to PDF</h3>
-              <p className="text-gray-300 text-center">Convert images to PDF effortlessly.</p>
-            </div>
-
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('compress')}
-            >
-              <Image
-                src="https://img.icons8.com/48/ffffff/compress.png"
-                alt="Compress PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Compress PDF</h3>
-              <p className="text-gray-300 text-center">Shrink PDFs for easy sharing.</p>
-            </div>
-
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('addpages')}
-            >
-              <Image
-                src="/addpages.png"
-                alt="Add Pages to PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Add Pages to PDF</h3>
-              <p className="text-gray-300 text-center">Insert new pages to your PDF.</p>
-            </div>
-
-            <div
-              className={`relative cursor-pointer overflow-hidden rounded-lg py-12 flex-col flex justify-center items-center text-center font-extrabold  mb-10  transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,155,1)] shadow-[inset_0_0_15px_rgba(0,0,155,0.7)] `}
-              onClick={() => handleButtonClick('extract')}
-            >
-              <Image
-                src="https://img.icons8.com/?size=100&id=wUqJRyU3Sxwa&format=png&color=000000"
-                alt="Extract Images from PDF"
-                width={48}
-                height={48}
-                className="mx-auto mb-4"
-              />
-              <h3 className="text-2xl font-semibold text-white text-center mb-2">Extract Images</h3>
-              <p className="text-gray-300 text-center">Easily extract images from PDFs.</p>
-            </div>
-
+          <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8">
+          {cardData.map((card) => (
+              <div
+                key={card.id}
+                className="relative cursor-pointer overflow-hidden rounded-lg px-2 md:px-0 py-5 md:py-12 flex-col flex justify-center items-center text-center font-extrabold transform hover:scale-[101%] transition-transform duration-200 ease-out tracking-wider hover:shadow-[inset_0_0_30px_rgba(0,0,255,1)] shadow-[inset_0_0_15px_rgba(0,0,255,0.7)]"
+                onClick={() => handleButtonClick(card.id)}
+              >
+                <Image
+                  src={card.imageSrc}
+                  alt={card.title}
+                  width={48}
+                  height={48}
+                  className="mx-auto mb-4"
+                />
+                <h3 className="text-lg md:text-2xl  font-semibold text-white text-center mb-2">{card.title}</h3>
+                <p className="text-gray-300 text-sm md:text-base font-normal text-center">{card.description}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 // import LandingPage from "./components/LandingPage";
 import OptionsPage from "./components/OptionsPage";
 import Landing from "./components/Landing"
+import Image from 'next/image';
 function Home() {
   const [activeSection, setActiveSection] = useState('home');
   const [getStarted, setGetStarted] = useState(false);
@@ -13,21 +14,73 @@ function Home() {
 
   return (
     <div
-    style={{
-      lineHeight: '1.5',
-      WebkitTextSizeAdjust: '100%',
-      fontFamily: 'system-ui, sans-serif',
-      WebkitFontSmoothing: 'antialiased',
-      textRendering: 'optimizeLegibility',
-      MozOsxFontSmoothing: 'grayscale',
-      touchAction: 'manipulation',
-      backgroundColor: '#111',
-      backgroundImage: 'radial-gradient(#2d2d2d 1px, transparent 0), radial-gradient(#373636 1px, transparent 0)',
-      backgroundPosition: '0 0, 25px 25px',
-      backgroundSize: '50px 50px',
-    }}
-     className="body-wrap m-0 min-h-screen  text-white font-sans text-body bg-[#1A202C] transition-colors duration-200 leading-6 text-base  box-border -webkit-font-smoothing:antialiased overflow-hidden flex flex-col bg-fixed">
+      style={{
+        lineHeight: '1.5',
+        WebkitTextSizeAdjust: '100%',
+        fontFamily: 'system-ui, sans-serif',
+        WebkitFontSmoothing: 'antialiased',
+        textRendering: 'optimizeLegibility',
+        MozOsxFontSmoothing: 'grayscale',
+        touchAction: 'manipulation',
+        backgroundColor: '#111',
+        backgroundImage: 'radial-gradient(#2d2d2d 1px, transparent 0), radial-gradient(#373636 1px, transparent 0)',
+        backgroundPosition: '0 0, 25px 25px',
+        backgroundSize: '50px 50px',
+      }}
+      className="body-wrap m-0 min-h-screen  text-white font-sans text-body bg-[#1A202C] transition-colors duration-200 leading-6 text-base  box-border -webkit-font-smoothing:antialiased overflow-hidden flex flex-col bg-fixed">
       {/* <LandingPage setGetStarted={setGetStarted} /> */}
+      <header className="w-full bg-transparent shadow-sm">
+        <div className="pb-2 pt-2 top-0 nav_gradient z-10 text-white xl:px-24 md:px-10 px-5">
+          <nav data-headlessui-state=""><div className="relative flex h-16">
+            <div className="absolute inset-y-0 right-0 flex items-center lg:hidden">
+              <button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" id="headlessui-disclosure-button-:r0:" type="button" aria-expanded="false" data-headlessui-state="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" className="block h-6 w-6 text-white">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5">
+                  </path>
+                </svg>
+              </button>
+            </div>
+            <div className="w-[100%] flex justify-between items-center">
+              <a rel="noreferrer" href="/">
+                <div className="flex items-center h-fit mt-2 -ml-4 md:ml-0">
+                  <Image width={2} height={2} src="/favicon.svg" className="mx-4 sm:ml-9 sm:h-5 w-15 md:h-10 w-10 " draggable="false" alt={''} />
+                  <h1 className="md:text-2xl tracking-normal font-bold font-sans sm:text-3xl xs:text-2xl text-xl text-white  whitespace-nowrap">
+                    PDF Manipulator
+                  </h1>
+                </div>
+              </a>
+              <div className="hidden text-xl lg:flex items-center">
+                <div className="flex space-x-1">
+                  <a target='_blank' href="https://github.com/VrajVyas11/Next_JS_PDF_Manipulator/blob/main/README.md">
+                    <p className=" text-white text-lg hover:text-[#7eb4e7] transition-all cursor-pointer ">
+                      Docs
+                    </p>
+                  </a>
+                </div>
+                <div className="flex flex-row justify-center items-center space-x-2 pl-10 -mt-6 md:-mt-0">
+                  <a href="https://github.com/VrajVyas11/Next_JS_PDF_Manipulator" target="_blank" rel="noreferrer">
+                    <img className=' w-10 h-auto' src="/images/github-logo.png" draggable="false" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/vraj-vyas-983249297/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="w-10 h-auto"
+                      src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000"
+                      alt="LinkedIn Icon"
+                      draggable="false"
+                    />
+                  </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          </nav>
+        </div>
+      </header>
       <Landing setGetStarted={setGetStarted} />
       {getStarted && (
         <OptionsPage handleButtonClick={handleButtonClick} activeSection={activeSection} />
