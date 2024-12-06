2935d4f29d68415d297fa24fce8e37aac425c1c8
Vraj Vyas <vyasvraj92@gmail.com>
2024-12-06 20:25:49 +0530

    moved files around

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { useActiveLink } from "./context/ContextProvider";
-const PDFEditor = dynamic(() => import('../components/PDFEditor/PDFEditor'), { ssr: false });
-const AddPages = dynamic(() => import('../components/AddPages'), { ssr: false });
-import PDFMerger from '../components/PDFMerge';
-import ImageToPDF from '../components/ImageToPDF';
-import CompressPDF from '../components/CompressPDF';
-import GetPDFImages from '../components/GetPDFImages';
+const PDFEditor = dynamic(() => import('../components/Core/PDFEditor/PDFEditor'), { ssr: false });
+const AddPages = dynamic(() => import('../components/Core/AddPages'), { ssr: false });
+import PDFMerger from '../components/Core/PDFMerge';
+import ImageToPDF from '../components/Core/ImageToPDF';
+import CompressPDF from '../components/Core/CompressPDF';
+import GetPDFImages from '../components/Core/GetPDFImages';
 import Home from "../components/Reworks/Home"
 function Base() {
   const { activeLink, setActiveLink } = useActiveLink();
