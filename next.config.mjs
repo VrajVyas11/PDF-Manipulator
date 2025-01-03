b4c64e7d347c5ba655526a3ae528d519dab25fc1
Vraj Vyas <vyasvraj92@gmail.com>
2025-01-03 16:54:42 +0530

    trying to fix vercel problem with canvas

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        canvas: false, // Stub the 'canvas' module
+      };
+      return config;
+    },
+  };
+  
 export default nextConfig;
